fab5117fbf073efe23e42cb2f6572560166ad8dc
Stepan Dordiai <stepandordiai@gmail.com>
2025-02-19 14:38:23 +0100

    v1.4.0

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,12 @@ import "./Home.scss";
 
 const Home = () => {
     return (
-        <>
-            <section className="home"></section>
-        </>
+        <section className="home">
+            <div>
+                <p>Vítejte na stránkách společnosti</p>
+                <h1 className="home__title">Neresen a.s.</h1>
+            </div>
+        </section>
     );
 };
 
