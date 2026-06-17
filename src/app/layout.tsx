e09a95fd7212bef3e1fb037f2af825e60d96f32e
Stepan Dordiai <stepandordiai@gmail.com>
2026-06-17 15:57:17 +0200

    feat: ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 				{children}
 				<Footer />
 				<Script
-					src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBzLUqCWNklkCcKNCUPP062nse38oOTaDM&loading=async"
+					src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&loading=async`}
 					strategy="afterInteractive"
 				/>
 			</body>
